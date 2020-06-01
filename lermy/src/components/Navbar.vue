7406d21eefe025f099967fb70739d13b47425b99
Lermy Ferrer <lermy.ferrer@insoftar.com>
2020-05-31 22:48:32 -0500

    adelanto de proyecto personal

---
+++ lermy/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-toolbar extended>
+    
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Ing Lermy Ferrer Peralta</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+      
+    </v-toolbar>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
